55d9074ffb9fdce6cde5e1ab48f3e02d96690aed
kevinrizkhy <kevin.rizkhy85@gmail.com>
2019-09-07 16:25:20 +0700

    sync

--- app/controllers/gets_controller.rb
+++ app/controllers/gets_controller.rb
@@ -1,18 +1,18 @@
 class GetsController < ApplicationController
 	def index
 		 json_result = {}
-		 render :json =>json_result if params[:type].nil? || params[:last].nil?
+		 render :json =>json_result if params[:type].nil? || params[:from].nil? || params[:to].nil?
 		 store = Store.find_by(id: params[:type])
-		 last_update = params[:last].to_date
-		 binding.pry
+		 from = params[:from].to_datetime.strftime('%a, %d %b %Y %H:%M:%S')
+		 to = params[:to].to_datetime.strftime('%a, %d %b %Y %H:%M:%S')
 		 render :json =>json_result if store.nil?
-		 json_result["stores"] = Store.where("updated_at >= ?", last_update)
-		 json_result["users"] = User.where("updated_at >= ?", last_update)
-		 json_result["departments"] = Department.where("updated_at >= ?", last_update)
-		 json_result["item_cats"] = ItemCat.where("updated_at >= ?", last_update)
-		 json_result["items"] = Item.where("updated_at >= ?", last_update)
-		 json_result["stocks"] = StoreItem.where(store: store).where("updated_at >= ?", last_update)
-		 json_result["grocers"] = GrocerItem.where("updated_at >= ?", last_update)
+		 json_result["stores"] = Store.where("updated_at >= ? AND updated_at <= ?", from, to)
+		 json_result["users"] = User.where("updated_at >= ? AND updated_at <= ?", from, to)
+		 json_result["departments"] = Department.where("updated_at >= ? AND updated_at <= ?", from, to)
+		 json_result["item_cats"] = ItemCat.where("updated_at >= ? AND updated_at <= ?", from, to)
+		 json_result["items"] = Item.where("updated_at >= ? AND updated_at <= ?", from, to)
+		 json_result["stocks"] = StoreItem.where("updated_at >= ? AND updated_at <= ?", from, to)
+		 json_result["grocers"] = GrocerItem.where("updated_at >= ? AND updated_at <= ?", from, to)
 		 render :json => json_result
 	end
 end
