1dc3f9cc42b182dfcb92e28ac0b95bd1e8fe93d9
kevinrizkhy <kevin.rizkhy85@gmail.com>
2019-10-04 13:52:17 +0700

    A

--- app/controllers/retur_items_controller.rb
+++ app/controllers/retur_items_controller.rb
@@ -25,7 +25,9 @@ class ReturItemsController < ApplicationController
     order = nil
     loss = nil
     receivable = nil
-    total_items = 0
+    cash_flow = nil
+    diff_stock_val_cash = 0
+    desc = ""
     return redirect_back_data_error returs_path, "Data Retur Tidak Valid" if feed_value.empty?
     
     feed_value.each do |value|
@@ -36,24 +38,13 @@ class ReturItemsController < ApplicationController
           order.delete
           break
       end
-      next if retur_item.accept_item <= 0
+      accept_item = retur_item.accept_item
+      next if accept_item <= 0
       if value[0] == "retur_item"
-        if order.nil?
-          order = Order.create supplier_id: retur.supplier_id, 
-            store: current_user.store, 
-            total_items: total_items,
-            total: 0,
-            date_created: DateTime.now,
-            invoice: "ORD-" + Time.now.to_i.to_s, 
-            editable: false,
-            user: current_user,
-            from_retur: true
-        end
-       
         order_qty = value[2].to_i
-        if order_qty != retur_item.accept_item
+        if order_qty != accept_item
           value[0] = "loss"
-          diff_item =  (retur_item.accept_item - order_qty)
+          diff_item =  (accept_item - order_qty)
           if loss.nil?
             invoice = "LOSS-" + Time.now.to_i.to_s
             loss = Loss.create user: current_user, store: current_user.store, from_retur: true, ref_id: retur.id, total_item: diff_item, invoice: invoice
@@ -66,6 +57,18 @@ class ReturItemsController < ApplicationController
         end
 
         if order_qty > 0
+          if order.nil?
+            order = Order.create supplier_id: retur.supplier_id, 
+              store: current_user.store, 
+              total_items: 0,
+              total: 0,
+              date_created: DateTime.now,
+              invoice: "ORD-" + Time.now.to_i.to_s, 
+              editable: false,
+              user: current_user,
+              from_retur: true
+          end
+
           ord_item = Item.find_by(id: retur_item.item.id)
           OrderItem.create  quantity: order_qty, 
                             price: 0,
@@ -75,39 +78,59 @@ class ReturItemsController < ApplicationController
 
           retur_item.ref_id = order.id
           retur_item.nominal = order_qty
-          total_items+=1
-          order.update(total_items: total_items)
+          order.total_items = order.total_items + order_qty
+          order.save!
         end
         
       elsif value[0] == "cash"
         nominal_value = value[2].to_i
 
         return redirect_back_data_error urls, "Nominal Potong Nota > 100" if nominal_value < 100
+
+        item = retur_item.item
+        diff_stock_val_cash += (nominal_value - (item.buy * accept_item))
         if receivable.nil?
+          desc = "RECEIVABLE FROM RETUR #"+retur.invoice
           receivable = Receivable.create user: current_user, store: current_user.store, nominal: nominal_value, date_created: DateTime.now, 
-                        description: "RECEIVABLE FROM RETUR #"+retur.invoice, finance_type: Receivable::RETUR, deficiency:nominal_value, to_user: retur.supplier_id,
+                        description: desc, finance_type: Receivable::RETUR, deficiency:nominal_value, to_user: retur.supplier_id,
                         ref_id: urls, due_date: DateTime.now + 2.months
         else
-          receivable.nominal += receivable.nominal+nominal_value
-          receivable.deficiency += receivable.deficiency+nominal_value
+          receivable.nominal = receivable.nominal+nominal_value
+          receivable.deficiency = receivable.deficiency+nominal_value
           receivable.save!
         end
         retur_item.ref_id = receivable.id
         retur_item.nominal = nominal_value
+
       elsif value[0] == "loss"
         if loss.nil?
-          invoice = "LOSS" + Time.now.to_i.to_s
-          Loss.create user: current_user, store: current_user.store, from_retur: true, ref_id: retur.id, total_item: 1, invoice: invoice
-          LossItem.create item: retur_item.item, quantity: retur_item.quantity, loss: loss, description: "LOSS FROM RETUR #"+retur.invoice
+          invoice = "LOSS-" + Time.now.to_i.to_s
+          Loss.create user: current_user, store: current_user.store, from_retur: true, ref_id: retur.id, total_item: accept_item, invoice: invoice
+          LossItem.create item: retur_item.item, quantity: accept_item, loss: loss, description: "LOSS FROM RETUR #"+retur.invoice
         else
-          loss.total_item = loss.total_item + 1
+          loss.total_item = loss.total_item + retur_item.quantity
           loss.save!
-          LossItem.create item: retur_item.item, quantity: retur_item.quantity, loss: loss, description: "LOSS FROM RETUR #"+retur.invoice
+          LossItem.create item: retur_item.item, quantity: accept_item, loss: loss, description: "LOSS FROM RETUR #"+retur.invoice
         end
+        retur_item.ref_id = receivable.id
       end
+
       retur_item.feedback = value[0]
       retur_item.save!
     end
+
+    if diff_stock_val_cash != 0
+      if diff_stock_val_cash > 0
+        invoice = " IN-"+Time.now.to_i.to_s
+        cash_flow_in = CashFlow.create user: user, store: store, nominal: diff_stock_val_cash, date_created: date_created, description: desc, 
+                      finance_type: CashFlow::INCOME, invoice: invoice
+      else
+        invoice = " OUT-"+Time.now.to_i.to_s
+        cash_flow_in = CashFlow.create user: user, store: store, nominal: diff_stock_val_cash, date_created: date_created, description: desc, 
+                      finance_type: CashFlow::INCOME, invoice: invoice
+      end
+    end
+
     retur.status = Time.now
     retur.save
     return redirect_success urls, "Data Retur " + retur.invoice + " Telah Dikonfirmasi"
