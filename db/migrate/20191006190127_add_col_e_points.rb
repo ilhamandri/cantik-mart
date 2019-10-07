79d65f42714db9aadc479b160c6b3c9b34f1a3d5
kevinrizkhy <kevin.rizkhy85@gmail.com>
2019-10-07 18:27:43 +0700

    asd

--- db/migrate/20191006190127_add_col_e_points.rb
+++ db/migrate/20191006190127_add_col_e_points.rb
@@ -6,7 +6,6 @@ class AddColEPoints < ActiveRecord::Migration[5.2]
   	end
   	add_column :exchange_points,:status, :boolean, null: false, default: true
   	add_reference :points, :voucher, foreign_key: true
-  	add_column :stores, :modals_before, :float, null: false, default: 0
   	add_reference :transactions, :voucher, foreign_key: true
     add_column :grocer_items, :member_price, :bigint, default: 0
   end
