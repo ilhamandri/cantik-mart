052cf105d8b53a73513786e1cc775eafadcb4439
kevinrizkhy <kevin.rizkhy85@gmail.com>
2019-11-10 18:36:22 +0700

    asd

--- app/controllers/apis_controller.rb
+++ app/controllers/apis_controller.rb
@@ -121,12 +121,9 @@ class ApisController < ApplicationController
     return render :json => json_result unless search.present?
     search = search.gsub(/\s+/, "")
     find_item = Item.find_by(code: search)
-    binding.pry
     return render :json => json_result unless find_item.present?
-    binding.pry
+    
     return render :json => json_result if find_item.local_item
-
-    binding.pry
     item = []
     item << find_item.code
     item << find_item.name
