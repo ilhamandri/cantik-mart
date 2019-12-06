2da2618870afa8afe147ea831405d5a53b342485
kevinrizkhy <kevin.rizkhy85@gmail.com>
2019-12-06 14:32:40 +0700

    asd

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -81,19 +81,13 @@ class PostsController < ApplicationController
 					absent = Absent.where(user_id: data["user_id"]).where("created_at >= ? AND created_at <= ?", start_date, end_date).first
 
 					data.delete("id")
-
-					binding.pry
 					
 					if absent.nil?
 						absent = Absent.create data
 					else
-						binding.pry
 						absent.assign_attributes data
-						binding.pry
 						absent.save!
 					end
-
-					binding.pry
 				end
 			end	
 		end
