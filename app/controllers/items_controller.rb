d1d12e927e91cdbf44f1323e7e3ae4da38cf9144
kevinrizkhy <kevin.rizkhy85@gmail.com>
2020-06-20 23:43:09 +0700

    asd

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -85,6 +85,7 @@ class ItemsController < ApplicationController
 
     raw_prices = ItemPrice.where(item: @item).where("created_at >= ?", DateTime.now-13.months).group(:month, :year).average(:buy)
     raw_prices.each do |item_price|
+      binding.pry
       month = Date::MONTHNAMES[item_price[0][0]] + " " + item_price[0][1].to_s
       b = item_price[1]
 
