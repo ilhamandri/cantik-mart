e982be73404c7a7ea2625fe94e3de7b3a35f5d9f
kevinrizkhy <kevin.rizkhy85@gmail.com>
2020-07-17 22:33:54 +0700

    asd

--- app/controllers/warning_items_controller.rb
+++ app/controllers/warning_items_controller.rb
@@ -81,7 +81,7 @@ class WarningItemsController < ApplicationController
         sheet.each do |row|
           next if sheet.first == row
           code = row[1]
-          binding.pry if code.nil?
+          next if code.nil?
           code = code.gsub(" ","")
           code = code.gsub("'","")
           item = Item.find_by(code: code)
@@ -97,7 +97,7 @@ class WarningItemsController < ApplicationController
           next if real_stock.nil? 
           # new_stock = curr_stock + (last_stock * -1) + real_stock
           new_stock = real_stock
-          if curr_stock <= 0
+          if curr_stock < 0
             new_stock -= curr_stock
             store_item.stock = new_stock
             store_item.save!
