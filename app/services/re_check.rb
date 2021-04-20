c4d1decbee5874235f795467cc79ec93b1d3b85a
kevinrizkhy <kevinrizkhy@gmail.com>
2021-04-20 09:28:25 +0700

    asd

--- app/services/re_check.rb
+++ app/services/re_check.rb
@@ -10,7 +10,7 @@ class ReCheck
 			trx1 = Transaction.find_by(id: trx2.complain.transaction_id)
 			trx1_complain_hpp = 0
 			trx1.transaction_items.each do |trx1_item|
-				trx1_complain_hpp += (trx1_item.item.buy*trx1_item.retur)
+				trx1_complain_hpp += (trx1_item.item.buy*trx1_item.retur) if trx1_item.retur.present?
 			end
 			trx2_hpp -= trx1_complain_hpp
 			trx2.save!
