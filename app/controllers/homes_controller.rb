f256e656de5dd210f363ad9efa601c479f5d04be
kevinrizkhy <kevinrizkhy@gmail.com>
2021-04-20 09:26:02 +0700

    asd

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -4,6 +4,7 @@ class HomesController < ApplicationController
 
   def index
     ItemUpdate.updateItem
+    ReCheck.complain
     @total_limit_items = StoreItem.where(store_id: current_user.store.id).where('stock < min_stock').count
     @total_orders = Order.where(store_id: current_user.store.id).where('date_receive is null').count
     @total_payments = Order.where(store_id: current_user.store.id).where('date_receive is not null and date_paid_off is null').count
---
+++ app/services/re_check.rb
@@ -0,0 +1,20 @@
+class ReCheck
+
+	def self.complain
+		end_date = DateTime.now
+		start_date = end_date - 1.day
+		trxs = Transaction.where(from_complain: true, created_at:start_date..end_date)
+		trxs.each do |trx2|
+			trx2_grand_total = trx2.grand_total
+			trx2_hpp = trx2.hpp_total
+			trx1 = Transaction.find_by(id: trx2.complain.transaction_id)
+			trx1_complain_hpp = 0
+			trx1.transaction_items.each do |trx1_item|
+				trx1_complain_hpp += (trx1_item.item.buy*trx1_item.retur)
+			end
+			trx2_hpp -= trx1_complain_hpp
+			trx2.save!
+		end
+	end
+
+end
