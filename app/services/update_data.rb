9ff0cf4f2a58abffc363962865d24f1b154d2d81
kevinrizkhy <kevin.rizkhy85@gmail.com>
2022-05-11 12:08:04 +0700

    coin

--- app/services/update_data.rb
+++ app/services/update_data.rb
@@ -4,6 +4,7 @@ class UpdateData
 		trx_items = TransactionItem.where(item_id: 30331)
 		trx_items.each do |trx_item|
 			trx = trx_item.trx
+			trx.has_coin = true
 			trx.grand_total_coin = trx_item.total
 			trx.hpp_total_coin = trx_item.item.buy * trx_item.quantity
 			trx.profit_coin = trx.grand_total_coin - trx.hpp_total_coin
