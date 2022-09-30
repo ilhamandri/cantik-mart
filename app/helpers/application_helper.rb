a045759d9ae69bc500b595ee1b2eb9d375fd5816
kevinrizkhy <kevin.rizkhy85@gmail.com>
2022-09-30 10:08:12 +0700

    button access

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+	def isAdmin
+		return true if ["super_admin", "owner"].include? current_user.level
+		return false
+	end
+
+	def buttonAccess controller_name, action_name
+		return true if isAdmin
+		can_access = Controller.find_by(name: controller_name).controller_methods.find_by(name: action_name).user_methods.pluck(:user_level).include? current_user.level
+		return true if can_access
+		return false
+	end
 end
