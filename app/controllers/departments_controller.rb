1232f521ec266f09793ca873515d563ef91f90d7
kevinrizkhy <kevin.rizkhy85@gmail.com>
2023-05-08 15:04:33 +0700

    popular item

--- app/controllers/departments_controller.rb
+++ app/controllers/departments_controller.rb
@@ -20,11 +20,7 @@ class DepartmentsController < ApplicationController
     return redirect_back_data_error departments_path, "Data Dapartemen Tidak Ditemukan" unless params[:id].present?
     @department = Department.find_by_id params[:id]
     return redirect_back_data_error departments_path, "Data Dapartemen Tidak Ditemukan" if @department.nil?
-    trxs = TransactionItem.where(department_id: @department).last(500)
-    items = {}
-    trxs.each do |trx|
-      trx
-    end
+    UpdateData.updatePopularItems
     respond_to do |format|
       format.html
       format.pdf do
