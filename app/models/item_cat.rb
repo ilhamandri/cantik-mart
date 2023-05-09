1a31f1129ac229b523ca994b5fa813a32e0f92a5
kevinrizkhy <kevin.rizkhy85@gmail.com>
2023-05-09 16:02:48 +0700

    user sync

--- app/models/item_cat.rb
+++ app/models/item_cat.rb
@@ -2,7 +2,7 @@ class ItemCat< ApplicationRecord
   validates :name, :department_id,  presence: true
   has_many :item
   belongs_to :department
-  default_scope { order(created_at: :desc) }
+  default_scope { order(name: :asc) }
   belongs_to :edited_by, class_name: "User", foreign_key: "edited_by", optional: true
 
 end
